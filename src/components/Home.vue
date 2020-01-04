c3e259506c92f1024dd1ff9aedca530db3ee24d3
theatre <oliverbin@live.com>
2020-01-04 23:05:18 +0800

    创建Home组件

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="home-container">home</div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>Header</el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -7,4 +17,16 @@ export default {}
 </script>
 
 <style lang="less" scoped>
+  .home-container {
+    height: 100%;
+  }
+  .el-header {
+    background-color: #373D41;
+  }
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main {
+    background-color: #fff;
+  }
 </style>
